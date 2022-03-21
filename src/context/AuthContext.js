8b5c11c8fd899975fff6615cea71fcda36ea6816
Maikon <m.carvalho@grouplinknetwork.com>
2022-03-21 16:34:18 -0300

    refactoring auth

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,45 @@
 import { createContext, useEffect, useState, useContext} from 'react';
-import {parseCookies, setCookie} from 'nookies';
+import { parseCookies, setCookie} from 'nookies';
+import { useRouter } from 'next/router';
+
+const Router = useRouter();
 
 export const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+
+    const isAuthenticated = false;
+
+    async function singIn (data) {
+        const { users, token } = await fetch(`/dye/v3/api/auth`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email:  data.email,
+                password: data.password,
+                remember: true
+                })
+             
+
+
+            })
+            // Create cookies with nookies
+            const cookies = parseCookies();
+            const { token } = await response.json();
+            setCookie(null, 'token', token, {
+                maxAge: 30 * 24 * 60 * 60,
+                path: '/',
+            });
+            setUser(
+                users
+            )
+            Router.push('/dashboard');
+            return token, users;
+    }
+    
+
     const [cart, setCart] = useState([]);
     const auth = 'authetication'
 
@@ -47,7 +83,7 @@ function getCart() {
     }, []);
 
     return (
-        <AuthContext.Provider value={{cart, handleAddtoCart, handleRemoveItemFromCart, clearCart, hadlesItemCartLength, getCart}}>
+        <AuthContext.Provider value={{singIn, cart, handleAddtoCart, handleRemoveItemFromCart, clearCart, hadlesItemCartLength, getCart}}>
             {children}
         </AuthContext.Provider>
     );
